c87cd7780d325a3c19b6fa13c107aa5c8c3032e6
Igle <javiglesias18@gmail.com>
2014-10-08 01:37:13 +0200

    add source
    now the source is added

---
+++ src/Agente4.java
@@ -0,0 +1,23 @@
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+
+
+public class Agente4 extends Agent 
+{
+	private static final long serialVerisonUID = 1L;
+	protected CyclicBehaviour cyclicBehaviour;
+		public void setup()
+		{
+			cyclicBehaviour = new CyclicBehaviour(this)
+			{
+
+				@Override
+				public void action() 
+				{
+					block();
+				}
+				
+			};
+			addBehaviour(cyclicBehaviour);
+		}
+}
